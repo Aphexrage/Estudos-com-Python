42183070e0ca27ff35a2ff79fad47e07dbe7202b
aphexrage <aphexragedev@gmail.com>
2025-11-17 16:05:03 -0300

    Subindo o restante

---
+++ POO/Exercicios do site/ex4/Aviao.py
@@ -0,0 +1,9 @@
+from Vehicle import *
+
+class Aviao(Vehicle):
+    pass
+
+Aviao = Vehicle
+Aviao("Jato", 4000, 1000)
+
+Aviao.passageiros(10)
--- POO/Exercicios do site/ex4/Vehicle.py
+++ POO/Exercicios do site/ex4/Vehicle.py
@@ -1,5 +1,10 @@
 class Vehicle:
-    def __init__(self, name, maxSpeed, milage):
+    def __init__(self, name, maxSpeed, mileage):
         self.name = name
         self.maxSpeed = maxSpeed
         self.mileage = mileage
+        return print(f"O nome do veiculo é {name}, velocidade max é {maxSpeed} e a quilometragem é de {mileage}")
+    
+    def passageiros(self, capacidade):
+        self.capacidade = capacidade
+        return print(f"A capacidade do veiculo é de {capacidade}")
