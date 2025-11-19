51c0e98dd2bcfcc188d7c17d99036612fde2f4b5
aphexrage <aphexragedev@gmail.com>
2025-11-18 22:03:09 -0300

    Subindo o ex8

---
+++ POO/Exercicios do site/ex8/main.py
@@ -0,0 +1,17 @@
+class Vehicle:
+    def __init__(self, name, maxSpeed, meliage):
+        self.name = name
+        self.maxSpeed = maxSpeed
+        self.meliage = meliage
+        
+class Teste:
+    def __init__(self, teste):
+        self.teste = teste
+        
+class Bus(Vehicle, Teste):
+    
+    onibus = Vehicle("Onibus", 100, 100000)
+    print(isinstance(onibus, Vehicle))
+    print(isinstance(onibus, Teste))
+    
+    
